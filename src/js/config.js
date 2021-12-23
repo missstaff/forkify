488a74d5315721153d954278555890fd78dc032c
Shawna Staff <staffs@my.lanecc.edu>
2021-12-23 01:25:53 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '881aada9-812c-49a5-b0ec-d90728ea5b68';
+export const MODAL_CLOSE_SEC = 2.5;
